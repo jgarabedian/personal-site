9459ed71c5b6c3ceb885a75502eba9df87456654
Jack Garabedian <jgarabedian96@gmail.com>
2020-12-15 20:42:29 -0500

    configure for heroku and hide pwd

--- email_sender.py
+++ email_sender.py
@@ -2,6 +2,9 @@ import smtplib
 from email.message import EmailMessage
 from string import Template
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def send_email(data) -> bool:
@@ -12,41 +15,40 @@ def send_email(data) -> bool:
     :return: {bool} True if success
     """
     # constants
-
     html = Template(Path('email_template.html').read_text())
     email = EmailMessage()
     email['to'] = 'fromjackg@gmail.com'
-
     name = data['name']
-    print(name)
     email['from'] = name
-
     email['subject'] = 'Message from ' + name
 
-    # email.set_content(html.substitute({'name': 'placeholder'}), 'html')
-    # print('content....')
     email.set_content(html.substitute({
         'name': name,
         'email': data['email'],
         'message': data['message'].strip()}), 'html'
     )
+
+    pwd = os.getenv('GMAIL_PWD')
+
     try:
         with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
             smtp.ehlo()
             smtp.starttls()
-            smtp.login('fromjackg@gmail.com', 'J@ckGar96')
+            smtp.login('fromjackg@gmail.com', pwd)
             smtp.send_message(email)
+            print('email sent')
         success = True
     except:
         success = False
         print('oh nooooooo')
     print(success)
+    return success
 
 
-if __name__ == '__main__':
-    data = {
-        'name': 'the file',
-        'email': 'fromjackg@gmail.com',
-        'message': 'placeholder'
-    }
-    send_email(data)
+# if __name__ == '__main__':
+#     data = {
+#         'name': 'the file',
+#         'email': 'fromjackg@gmail.com',
+#         'message': 'placeholder'
+#     }
+#     send_email(data)
